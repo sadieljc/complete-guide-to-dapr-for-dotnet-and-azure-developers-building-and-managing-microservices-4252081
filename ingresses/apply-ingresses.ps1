50229e7d12033b925be0a4003399afa8b6f09a53
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2025-08-15 15:40:40 -0700

    18_07e

---
+++ ingresses/apply-ingresses.ps1
@@ -0,0 +1,5 @@
+kubectl apply -f wisdom-hospital-api.yaml
+kubectl apply -f wisdom-pet-api.yaml
+kubectl apply -f wisdom-petaggregator-api.yaml
+kubectl apply -f wisdom-rescue-api.yaml
+kubectl apply -f wisdom-rescuequery-api.yaml
