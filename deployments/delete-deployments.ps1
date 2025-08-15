1d77d975e2dfe2d190c616b73ac9318716a2bbed
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2025-08-15 13:33:04 -0700

    18_05e

---
+++ deployments/delete-deployments.ps1
@@ -0,0 +1,5 @@
+kubectl delete -f wisdom-hospital-api.yaml
+kubectl delete -f wisdom-pet-api.yaml
+kubectl delete -f wisdom-petaggregator-api.yaml
+kubectl delete -f wisdom-rescue-api.yaml
+kubectl delete -f wisdom-rescuequery-api.yaml
