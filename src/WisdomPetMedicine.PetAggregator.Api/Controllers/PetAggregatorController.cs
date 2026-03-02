a10cd681df9bf7f2601fe58bddbe12bcd62b0768
Sadiel <sadieljc@hotmail.com>
2026-03-02 17:26:50 -0500

    Added new PetAggregator project

---
+++ src/WisdomPetMedicine.PetAggregator.Api/Controllers/PetAggregatorController.cs
@@ -0,0 +1,32 @@
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using WisdomPetMedicine.PetAggregator.Api.Models;
+
+namespace WisdomPetMedicine.PetAggregator.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PetAggregatorController : ControllerBase
+{
+    private readonly DaprClient daprClient;
+
+    public PetAggregatorController(DaprClient daprClient)
+    {
+        this.daprClient = daprClient;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var pets = await daprClient.InvokeMethodAsync<IEnumerable<PetModel>>(HttpMethod.Get, 
+                        "pet", "petquery");
+
+        var rescues = await daprClient.InvokeMethodAsync<IEnumerable<RescueModel>>(HttpMethod.Get,
+                        "rescuequery", "rescuequery");
+
+        var patients = await daprClient.InvokeMethodAsync<IEnumerable<PatientModel>>(HttpMethod.Get,
+                    "hospital", "patientquery");
+
+        return Ok();
+    }
+}
